22d1c041efbf8c68d6f3b2511b8bb8653c6f74d1
Nav <navdeep_aulakh24@hotmail.com>
2023-02-26 19:56:49 -0500

    finding all categories for category-routesjs

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,22 +3,55 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  await Category.findAll({
+    attributes: ['id', 'category_name'],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+      },
+    ],
+  })
+    .then((dbCategoryData) => res.json(dbCategoryData))
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+await Category.findByPk(req.params.id, {
+  attributes: ['id', 'category_name'],
+  include: [
+    {
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+    },
+  ],
+})
+  .then((dbCategoryData) => {
+    if (!dbCategoryData) {
+      res.status(404).json({ message: 'No category found with this id' });
+      return;
+    }
+    res.json(dbCategoryData);
+  })
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+await Category.create(req.body)
+  .then((dbCategoryData) => res.json(dbCategoryData))
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  
 });
 
 router.delete('/:id', (req, res) => {
