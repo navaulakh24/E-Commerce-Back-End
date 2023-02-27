48c0484337857e338e1e9ef27d6f798464d056aa
Nav <navdeep_aulakh24@hotmail.com>
2023-02-26 20:31:20 -0500

    finished delete tag route

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -77,6 +77,18 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((tagDeleted) => {
+      res.json(tagDeleted)
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
